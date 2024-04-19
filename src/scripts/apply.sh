ad7140f7656fd57e0648971bb3272dbef17830e2
ajmartin <adam.martin@pediatrix.com>
2024-04-19 10:03:20 -0400

    Fixing SC2155 (warning)

--- src/scripts/apply.sh
+++ src/scripts/apply.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 # This uses envsubst to support variable substitution in the string parameter type.
 # https://circleci.com/docs/orbs-best-practices/#accepting-parameters-as-strings-or-environment-variables
-export TF_VAR_app_registration_id=$(circleci env subst "${PARAM_APP_REGISTRATION_ID}")
-export TF_VAR_resource_group_name=$(circleci env subst "${PARAM_RESOURCE_GROUP_NAME}")
-export TF_VAR_key_vault_name=$(circleci env subst "${PARAM_KEY_VAULT_NAME}")
-export TF_VAR_key_name=$(circleci env subst "${PARAM_KEY_NAME}")
+TF_VAR_app_registration_id=$(circleci env subst "${PARAM_APP_REGISTRATION_ID}")
+TF_VAR_resource_group_name=$(circleci env subst "${PARAM_RESOURCE_GROUP_NAME}")
+TF_VAR_key_vault_name=$(circleci env subst "${PARAM_KEY_VAULT_NAME}")
+TF_VAR_key_name=$(circleci env subst "${PARAM_KEY_NAME}")
+export TF_VAR_app_registration_id
+export TF_VAR_resource_group_name
+export TF_VAR_key_vault_name
+export TF_VAR_key_name
 
 echo "App Registration = ${TF_VAR_app_registration_id}!"
 echo "Resource Group Name = ${TF_VAR_resource_group_name}!"
