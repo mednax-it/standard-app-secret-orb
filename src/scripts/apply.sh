7dd6ae2d027e4c75f22ae4f3b03b6300042fcc34
ajmartin <adam.martin@pediatrix.com>
2024-04-19 09:58:01 -0400

    Fixing a stupid error

--- src/scripts/apply.sh
+++ src/scripts/apply.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # This uses envsubst to support variable substitution in the string parameter type.
 # https://circleci.com/docs/orbs-best-practices/#accepting-parameters-as-strings-or-environment-variables
-TF_VAR_app_registration_id=$(circleci env subst "${PARAM_APP_REGISTRATION_ID}")
-TF_VAR_resource_group_name=$(circleci env subst "${PARAM_RESOURCE_GROUP_NAME}")
-TF_VAR_key_vault_name=$(circleci env subst "${PARAM_KEY_VAULT_NAME}")
-TF_VAR_key_name=$(circleci env subst "${PARAM_KEY_NAME}")
+export TF_VAR_app_registration_id=$(circleci env subst "${PARAM_APP_REGISTRATION_ID}")
+export TF_VAR_resource_group_name=$(circleci env subst "${PARAM_RESOURCE_GROUP_NAME}")
+export TF_VAR_key_vault_name=$(circleci env subst "${PARAM_KEY_VAULT_NAME}")
+export TF_VAR_key_name=$(circleci env subst "${PARAM_KEY_NAME}")
 
 echo "App Registration = ${TF_VAR_app_registration_id}!"
 echo "Resource Group Name = ${TF_VAR_resource_group_name}!"
