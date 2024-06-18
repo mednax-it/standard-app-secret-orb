17ac6e94a6a7427fdd4701aeeef7f9f518aa9e1e
Rob Miller <rob.miller@pediatrix.com>
2024-06-18 09:01:01 -0500

    Allows app registration display name to be specified in order to determine which app reg to apply secret to (#5)
    Allows app registration display name to be specified

--- src/scripts/apply.sh
+++ src/scripts/apply.sh
@@ -10,19 +10,12 @@ set -e
 AZURE_TENANT=$(circleci env subst "${AZURE_TENANT}")
 AZURE_SERVICE_PRINCIPLE=$(circleci env subst "${AZURE_SERVICE_PRINCIPLE}")
 AZURE_SERVICE_PRINCIPLE_PASSWORD=$(circleci env subst "${AZURE_SERVICE_PRINCIPLE_PASSWORD}")
-TF_VAR_app_registration_id=$(circleci env subst "${PARAM_APP_REGISTRATION_ID}")
-TF_VAR_resource_group_name=$(circleci env subst "${PARAM_RESOURCE_GROUP_NAME}")
-TF_VAR_key_vault_name=$(circleci env subst "${PARAM_KEY_VAULT_NAME}")
-TF_VAR_key_name=$(circleci env subst "${PARAM_KEY_NAME}")
 SUBSCRIPTION=$(circleci env subst "${SUBSCRIPTION}")
+
 export AZURE_TENANT
 export AZURE_SERVICE_PRINCIPLE
 export AZURE_SERVICE_PRINCIPLE_PASSWORD
 export SUBSCRIPTION
-export TF_VAR_app_registration_id
-export TF_VAR_resource_group_name
-export TF_VAR_key_vault_name
-export TF_VAR_key_name
 
 echo "Logging in to az cli"
 az login --service-principal \
@@ -30,6 +23,30 @@ az login --service-principal \
     -u "$AZURE_SERVICE_PRINCIPLE" \
     -p "$AZURE_SERVICE_PRINCIPLE_PASSWORD"
 az account set --subscription "$SUBSCRIPTION"
+
+TF_VAR_resource_group_name=$(circleci env subst "${PARAM_RESOURCE_GROUP_NAME}")
+TF_VAR_key_vault_name=$(circleci env subst "${PARAM_KEY_VAULT_NAME}")
+TF_VAR_key_name=$(circleci env subst "${PARAM_KEY_NAME}")
+
+export TF_VAR_resource_group_name
+export TF_VAR_key_vault_name
+export TF_VAR_key_name
+
+if [[ -z "$PARAM_APP_REGISTRATION_ID" ]]; then
+    echo "Retrieve app registration id for application ${PARAM_APP_REGISTRATION_DISPLAY_NAME}"
+    TF_VAR_app_registration_id=$(az ad app list --display-name "${PARAM_APP_REGISTRATION_DISPLAY_NAME}" --query '[0].appId' -o tsv)
+else
+    TF_VAR_app_registration_id=$(circleci env subst "${PARAM_APP_REGISTRATION_ID}")
+fi
+
+export TF_VAR_app_registration_id
+
+if [[ -z "$TF_VAR_app_registration_id" ]]; then
+    echo "Error: unable to determine app registration id"
+    exit 1
+else
+    echo "App registration id: $TF_VAR_app_registration_id"
+fi
 
 echo "Cloning terraform repo"
 git clone https://github.com/mednax-it/standard-app-secret-generation.git
